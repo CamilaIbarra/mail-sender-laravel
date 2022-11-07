07501dde0cb63f3542276e5c10847e8abd112e74
Camila Ibarra <ibarra.camiailen9@gmail.com>
2022-11-07 17:09:41 -0300

    First commit

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,2 @@
+<h1>Mail from mail-sender-laravel</h1>
+<p>Test mail</p>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function(){
+    Mail::to('test@email.com')->send(new TestMail());
+    return view('emails.message');
+});
